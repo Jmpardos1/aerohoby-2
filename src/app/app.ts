7b4b1abb40b888e990f6a825b4c4a2edd421f0c5
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2026-04-20 10:14:11 -0500

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ISIS2603_202610_S2_E4_Aerohobby_Front');
+}
