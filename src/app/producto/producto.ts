12fad73d6ad4b01f001cb3573c12665855fe2d68
JCO2006 <j.ojedav@uniandes.edu.co>
2026-04-26 21:09:01 -0500

    progreso

--- src/app/producto/producto.ts
+++ src/app/producto/producto.ts
@@ -4,4 +4,25 @@
 
 
 export class Producto {
+
+    id: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    stockMinimo: number;
+    marcaId: number;
+    prooveedorId: number;
+
+    constructor(id: number, nombre: string, descripcion: string, precio: number, stock: number, stockMinimo: number, marcaId: number, prooveedorId: number) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.stockMinimo = stockMinimo;
+        this.marcaId = marcaId;
+        this.prooveedorId = prooveedorId;
+    }
+    
 }
