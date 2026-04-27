2279b058b75e7c8be0ab7f67de1cda9c0600e9ee
JCO2006 <j.ojedav@uniandes.edu.co>
2026-04-26 19:57:11 -0500

    progreso

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,9 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductoModule } from './producto/producto.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductoModule,
+    HttpClientModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/producto/producto.ts
@@ -0,0 +1,7 @@
+
+
+// DTO TIENE LOS ATRIBUTOS DE LA CLASE
+
+
+export class Producto {
+}
