9fa1288b3f982ad2b4a971cc35c1249000e670c4
Jesus Rivera <ja.riverah1@uniandes.edu.co>
2026-05-03 23:48:46 -0500

    visualizacion ordenes compra

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './usuario/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,11 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('ISIS2603_202610_S2_E4_Aerohobby_Front');
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
